663ba5e6681d45c6b2f2a67d9407c28f7b204855
MelissaAlvarezCastro <37911350+MelissaAlvarezCastro@users.noreply.github.com>
2019-07-03 17:35:37 -0500

    categorias

--- src/lexico/App.java
+++ src/lexico/App.java
@@ -4,9 +4,10 @@ public class App {
 
 	public static void main(String[] args) {
 
-		String codigoFuente = "||AA1|:>:";
+		String codigoFuente = "||AA1|:-->:";
 		AnalizadorLexico al = new AnalizadorLexico(codigoFuente);
 		al.analizar();
+		System.out.println(codigoFuente);
 		System.out.println(al.getListaTokens());
 
 	}
